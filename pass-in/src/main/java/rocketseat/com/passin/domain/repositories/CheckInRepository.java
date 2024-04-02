033a435c359067e9db24c859c3bf6c59d14dd108
Bruno <38993322+NewarkX@users.noreply.github.com>
2024-04-02 20:24:13 -0300

    Criado service de attendees e o dto

--- pass-in/src/main/java/rocketseat/com/passin/domain/repositories/CheckInRepository.java
+++ pass-in/src/main/java/rocketseat/com/passin/domain/repositories/CheckInRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import rocketseat.com.passin.domain.attendee.Attendee;
 import rocketseat.com.passin.domain.checkin.CheckIn;
 
+import java.util.Optional;
+
 public interface CheckInRepository extends JpaRepository<CheckIn, Integer> {
+    Optional<CheckIn> findByAttendeeId(String attendeeId);
 }
--- pass-in/src/main/java/rocketseat/com/passin/services/AttendeeService.java
+++ pass-in/src/main/java/rocketseat/com/passin/services/AttendeeService.java
@@ -2,16 +2,33 @@ package rocketseat.com.passin.services;
 
 import org.springframework.stereotype.Service;
 import rocketseat.com.passin.domain.attendee.Attendee;
+import rocketseat.com.passin.domain.checkin.CheckIn;
 import rocketseat.com.passin.domain.repositories.AttendeeRepository;
+import rocketseat.com.passin.domain.repositories.CheckInRepository;
+import rocketseat.com.passin.dto.attendee.AttendeeDetails;
+import rocketseat.com.passin.dto.attendee.AttendeesListResponseDto;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AttendeeService {
     private AttendeeRepository attendeeRepository;
+    private CheckInRepository checkInRepository;
 
     public List<Attendee> getAllAttendeesFromEvent(String eventId){
         List<Attendee> attendeesList = this.attendeeRepository.findByEventId(eventId);
         return attendeesList;
+    }
+
+    public AttendeesListResponseDto getEventsAttendee(String eventId){
+        List<Attendee> attendeeList = this.getAllAttendeesFromEvent(eventId);
+        List<AttendeeDetails> attendeeDetailsList = attendeeList.stream().map(attendee -> {
+            Optional<CheckIn> checkIn = this.checkInRepository.findByAttendeeId(attendee.getId());
+            LocalDateTime checkedInAt = checkIn.isPresent() ? checkIn.get().getCreatedAt() : null;
+            return new AttendeeDetails(attendee.getId(), attendee.getName(),attendee.getEmail(),attendee.getCreatedAt(),checkedInAt);
+        }).toList();
+        return new AttendeesListResponseDto(attendeeDetailsList);
     }
 }
