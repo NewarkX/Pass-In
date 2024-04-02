ab18b1e940f94767cb1658c4d2838fa227960311
Bruno <38993322+NewarkX@users.noreply.github.com>
2024-04-01 21:38:23 -0300

    Entidades e Repositorios Criados

---
+++ pass-in/src/main/java/rocketseat/com/passin/controllers/EventController.java
@@ -0,0 +1,8 @@
+package rocketseat.com.passin.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EventController {
+    
+}
---
+++ pass-in/src/main/java/rocketseat/com/passin/domain/event/Event.java
@@ -0,0 +1,27 @@
+package rocketseat.com.passin.domain.event;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "events")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Event {
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    @Column(nullable = false)
+    private String tittle;
+    @Column(nullable = false)
+    private String details;
+    @Column(nullable = false,unique = true)
+    private String slug;
+    @Column(nullable = false,name = "maximum_attendees")
+    private Integer maximumAttendees;
+
+}
