989f15064c002ed717d0e35df84c9c97ed67e4ce
Bruno <38993322+NewarkX@users.noreply.github.com>
2024-04-01 21:48:31 -0300

    criado o esqueleto do controller de evento e participante

--- pass-in/src/main/java/rocketseat/com/passin/controllers/AteendeeController.java
+++ pass-in/src/main/java/rocketseat/com/passin/controllers/AteendeeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/events")
-public class EventController {
+@RequestMapping("/attendees")
+public class AteendeeController {
     @GetMapping
     public ResponseEntity<String> getTeste(){
         return ResponseEntity.ok("sucesso");
