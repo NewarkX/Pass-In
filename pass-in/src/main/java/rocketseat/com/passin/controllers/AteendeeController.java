fffbb240ad61c18ce9626078b4d1899e38a08c9b
Bruno <38993322+NewarkX@users.noreply.github.com>
2024-04-04 14:49:30 -0300

    endpoint que retorna o cracha do participante

--- pass-in/src/main/java/rocketseat/com/passin/controllers/AteendeeController.java
+++ pass-in/src/main/java/rocketseat/com/passin/controllers/AteendeeController.java
@@ -1,15 +1,25 @@
 package rocketseat.com.passin.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+import rocketseat.com.passin.dto.event.AttendeeBadgeResponseDto;
+import rocketseat.com.passin.services.AttendeeService;
 
 @RestController
 @RequestMapping("/attendees")
+@RequiredArgsConstructor
 public class AteendeeController {
-    @GetMapping
-    public ResponseEntity<String> getTeste(){
-        return ResponseEntity.ok("sucesso");
+    private final AttendeeService attendeeService;
+
+
+    @GetMapping("/{attendeeId}/badge")
+    public ResponseEntity<AttendeeBadgeResponseDto> getAttendeeBadge(@PathVariable String attendeeId, UriComponentsBuilder uriComponentsBuilder){
+        AttendeeBadgeResponseDto attendeeBadgeResponseDto = this.attendeeService.getAttendeeBadge(attendeeId,uriComponentsBuilder);
+        return ResponseEntity.ok(attendeeBadgeResponseDto);
     }
 }
