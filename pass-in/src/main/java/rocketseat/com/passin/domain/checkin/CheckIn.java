a923918ed411561d3c5ff13d4946428cce463986
Bruno <38993322+NewarkX@users.noreply.github.com>
2024-04-04 15:20:55 -0300

    registrar checkin ok e exception ok

--- pass-in/src/main/java/rocketseat/com/passin/domain/checkin/CheckIn.java
+++ pass-in/src/main/java/rocketseat/com/passin/domain/checkin/CheckIn.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import rocketseat.com.passin.domain.attendee.Attendee;
 
 import java.time.LocalDateTime;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "check_ins")
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class CheckIn {
